c110aa9fc37b3b84900c31678d6906b34ecbfcc6
Sebastian Bernasek <sebastian.bernasek@emeraldcloudlab.com>
2021-05-03 18:51:13 -0700

    Incremented version number to 0.2

--- setup.py
+++ setup.py
@@ -239,7 +239,7 @@ if USE_CYTHON:
 
 setup(
     name = "binding",
-    version = '0.1',
+    version = '0.2',
     author = "Sebastian Bernasek",
     author_email = "sebastian@u.northwestern.edu",
     url = "https://github.com/sebastianbernasek/binding",
